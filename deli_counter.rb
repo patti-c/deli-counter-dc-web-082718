df9ea7150228aa48fc87c150866b9a8e32d49915
Patricia Carter <patricia4194@gmail.com>
2018-08-07 22:41:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(katz_deli)
   if katz_deli == []
     return "The line is currently empty."
   else
-    puts "The line is currently:"
-    
+    print "The line is currently:"
+    katz_deli.each_ do |customer
   end 
 end 
   
