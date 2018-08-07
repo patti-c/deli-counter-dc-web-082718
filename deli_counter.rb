fedc95f7bb482705f3c4a41480d4ac677db35431
Patricia Carter <patricia4194@gmail.com>
2018-08-07 22:57:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,11 @@ def take_a_number(katz_deli, name)
 end 
 
 def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift}."
+  
+  
   katz_deli == [] ? puts "There is nobody waiting to be served!" : puts "Currently serving #{katz_deli.shift};
 end 
