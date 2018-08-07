ce210b3f1603ad84cad92204971395f341d6abdf
Patricia Carter <patricia4194@gmail.com>
2018-08-07 22:42:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(katz_deli)
     return "The line is currently empty."
   else
     print "The line is currently:"
-    katz_deli.each_ do |customer
+    katz_deli.each_with_index do |customer, index|
+      print "#{index}. #{customer}"
   end 
 end 
   
