b9a9710beadc33a4498e295f3b1d2f00c0cb6b5f
Patricia Carter <patricia4194@gmail.com>
2018-08-07 22:59:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    var line_sentence = "The line is currently:"
+    line_sentence = "The line is currently: "
     katz_deli.each_with_index do |customer, index|
-      print "#{index}. #{customer}"
+      line_sentence = line_sentence + "#{index}. #{customer} "
     end 
   end 
 end 
