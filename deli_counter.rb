8652b6e03c9264121f0956adb8afd3137f9a6ab8
Patricia Carter <patricia4194@gmail.com>
2018-08-07 22:43:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,10 @@ def line(katz_deli)
   end 
 end 
 
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end 
+
+def now_serving(katz_deli)
+  
+end 
